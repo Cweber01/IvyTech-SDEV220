9a7f9822db2daefd42934cb2d23a449083a695c8
Cweber01 <50115698+Cweber01@users.noreply.github.com>
2022-06-30 10:11:41 -0400

    Add files via upload

---
+++ M03Tutorial_Functional_vs_OOP.py
@@ -0,0 +1,58 @@
+# M03 Tutorial - Functional vs OOP Programming 130
+# Charles
+# This code will add objects to an iterable list and return an output
+
+class Pets():
+    animals = []
+    def __init__(self, animals):
+        self.animals = animals
+
+    def walk(self):
+        for animal in self.animals:
+            print(animal.walk())
+
+'''
+    def sing(self):
+        for animal in self.animals:
+            print (animal.sing())
+'''
+
+class Cat():
+    is_lazy = True
+
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def walk(self):
+        return f'{self.name} is just walking around'
+    
+'''
+    def sing(self):
+        return f'{self.name} says {sounds}'
+'''
+
+class Simon(Cat):
+    def sing(self, sounds):
+        return f'{self.sounds}'
+
+class Sally(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+#1 Add nother Cat
+class Henry(Cat):
+    def sing(self, sounds):
+        return f'{sounds}'
+
+
+#2 Create a list of all of the pets (create 3 cat instances from the above)
+my_cats = [Simon('Simon', 5), Sally('Sally', 7), Henry('Henry',3)]
+
+#3 Instantiate the Pet class with all your cats use variable my_pets
+my_pets = Pets(my_cats)
+
+
+#4 Output all of the cats walking using the my_pets instance
+my_pets.walk()
+#my_pets.sing()
